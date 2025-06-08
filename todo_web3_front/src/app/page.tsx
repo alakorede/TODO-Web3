1a975a19681a4e1441275e84ae874dfe5676d7f1
Luiz Moraes <luiz.moraes@tvfsoftware.com>
2025-06-07 22:40:03 -0300

    Updating config files

--- todo_web3_front/src/app/page.tsx
+++ todo_web3_front/src/app/page.tsx
@@ -1,106 +1,163 @@
-import { StakeCard } from "@/components/comons/stakeCard";
-import { StatusCard } from "@/components/comons/statusCard";
-import { TaskCard } from "@/components/comons/taskCard";
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, DialogContent} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { PlusIcon, WalletIcon } from "lucide-react";
-
+"use client"
+import { StakeCard } from "@/components/comons/stakeCard"
+import { StatusCard } from "@/components/comons/statusCard"
+import { TaskCard } from "@/components/comons/taskCard"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogContent
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import { PlusIcon, WalletIcon, XIcon } from "lucide-react"
+import { useEffect, useState } from "react"
+import { createWalletClient, custom } from "viem"
+import { anvil } from "viem/chains"
 
 const tasks = [
     {
         title: "Tarefa 1",
         description: "Descrição da tarefa 1",
-        createdAt:"2025-01-01",
-        dueDate:"2025-01-07",
-        stake:50
+        createdAt: "2025-01-01",
+        dueDate: "2025-01-07",
+        stake: 50
     },
-        {
+    {
         title: "Tarefa 2",
         description: "Descrição da tarefa 2",
-        createdAt:"2025-01-01",
-        dueDate:"2025-01-07",
-        stake:70
+        createdAt: "2025-01-01",
+        dueDate: "2025-01-07",
+        stake: 70
     },
-        {
+    {
         title: "Tarefa 3",
         description: "Descrição da tarefa 3",
-        createdAt:"2025-01-01",
-        dueDate:"2025-01-07",
-        stake:80
+        createdAt: "2025-01-01",
+        dueDate: "2025-01-07",
+        stake: 80
     },
-        {
+    {
         title: "Tarefa 4",
         description: "Descrição da tarefa 4",
-        createdAt:"2025-01-01",
-        dueDate:"2025-01-07",
-        stake:10
+        createdAt: "2025-01-01",
+        dueDate: "2025-01-07",
+        stake: 10
     }
 ]
 
 export default function Home() {
-return (
-    <div className="flex flex-col min-h-screen max-w-7xl mx-auto pt-10">
-        <div className="flex justify-between items-center p-4">
-            <div className="flex flex-col gap-2">
-                <h1 className="text-2xl bold">Web3 ToDo List</h1>
-                <h2 className="text-sm text-muted-foreground">Gerencie suas tarefas com Web3 (Segurança e confiança)</h2>
-            </div>
-            <Button><WalletIcon/><span>Conectar Wallet</span></Button>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
-            <StatusCard title="Total de Tarefas" value={10} />
-            <StatusCard title="Total de Tarefas concluídas" value={20}/>
-            <StatusCard title="Total de Tarefas pendentes" value={30}/>
-            <StatusCard title="Total de wei em stake" value={40}/>
-        </div>
-        <div className="flex justify-between items-center mt-10">
-            <h1 className="text-2xl bold">Tarefas</h1>
-            <Dialog>
-                <DialogTrigger>
-                    <Button>
-                        <PlusIcon/>
-                        <span>Nova Tarefa</span>
+    const [walletClient, setWalletClient] = useState<any>(null)
+    const [address, setAddress] = useState<string | null>(null)
+
+    useEffect(() => {
+        const client = createWalletClient({
+            chain: anvil,
+            transport: custom((window as any).ethereum!)
+        })
+        setWalletClient(client)
+    }, [])
+
+    const connectWallet = async () => {
+        if (!(window as any).ethereum) return
+        const [address] = await walletClient.requestAddresses()
+        setAddress(address)
+    }
+
+    const disconnectWallet = async () => {
+        setAddress(null)
+    }
+
+    return (
+        <div className="flex flex-col min-h-screen max-w-7xl mx-auto pt-10">
+            <div className="flex justify-between items-center p-4">
+                <div className="flex flex-col gap-2">
+                    <h1 className="text-2xl bold">Web3 ToDo List</h1>
+                    <h2 className="text-sm text-muted-foreground">
+                        Gerencie suas tarefas com Web3 (Segurança e confiança)
+                    </h2>
+                </div>
+                {!address ? (
+                    <Button onClick={connectWallet} className="cursor-pointer">
+                        <WalletIcon />
+                        <span>Conectar Wallet</span>
                     </Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Nova Tarefa</DialogTitle>
-                    </DialogHeader>
-                    <DialogDescription className="flex flex-col gap-4">
-                        <Label>Título</Label>
-                        <Input type="text" placeholder="Título da Tarefa"/>
-                        <Label>Descrição</Label>
-                        <Textarea placeholder="Decrição da Tarefa"/>
-                        <Label>Data de vencimento</Label>
-                        <Input type="datetime-local" placeholder="Data de Vencimento" />
-                        <div className="grid frid-cols-1 md:grid-cols-2 gap-4">
-                            <StakeCard/>
-                            <StakeCard/>
-                            <StakeCard/>
-                            <StakeCard/>
-                        </div>
-                        <Button>
-                            <PlusIcon/>
-                            <span>Criar Tarefa</span>
+                ) : (
+                    <div className="flex items-center gap-2">
+                        <span>{address}</span>
+                        <Button onClick={disconnectWallet}>
+                            <XIcon />
                         </Button>
-                    </DialogDescription>
-                </DialogContent>
-            </Dialog>
-        </div>
-        <div className="flex flex-col gap-4 mt-10">
-            {
-                tasks.length === 0 ? (
-                    <div className="flex justify-center items-center h-full">
-                        <h1 className="text-2xl font-bold">Nenhuma tarefa encontrada</h1>
                     </div>
-                ) : (tasks.map((task) => (
-                <TaskCard key={task.title} title={task.title} description={task.description} createdAt={task.createdAt} dueDate={task.dueDate} stake={task.stake}/>
-                )))
-            }
+                )}
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
+                <StatusCard title="Total de Tarefas" value={10} />
+                <StatusCard title="Total de Tarefas concluídas" value={20} />
+                <StatusCard title="Total de Tarefas pendentes" value={30} />
+                <StatusCard title="Total de wei em stake" value={40} />
+            </div>
+            <div className="flex justify-between items-center mt-10">
+                <h1 className="text-2xl bold">Tarefas</h1>
+                <Dialog>
+                    <DialogTrigger>
+                        <Button>
+                            <PlusIcon />
+                            <span>Nova Tarefa</span>
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Nova Tarefa</DialogTitle>
+                        </DialogHeader>
+                        <DialogDescription className="flex flex-col gap-4">
+                            <Label>Título</Label>
+                            <Input type="text" placeholder="Título da Tarefa" />
+                            <Label>Descrição</Label>
+                            <Textarea placeholder="Decrição da Tarefa" />
+                            <Label>Data de vencimento</Label>
+                            <Input
+                                type="datetime-local"
+                                placeholder="Data de Vencimento"
+                            />
+                            <div className="grid frid-cols-1 md:grid-cols-2 gap-4">
+                                <StakeCard />
+                                <StakeCard />
+                                <StakeCard />
+                                <StakeCard />
+                            </div>
+                            <Button>
+                                <PlusIcon />
+                                <span>Criar Tarefa</span>
+                            </Button>
+                        </DialogDescription>
+                    </DialogContent>
+                </Dialog>
+            </div>
+            <div className="flex flex-col gap-4 mt-10">
+                {tasks.length === 0 ? (
+                    <div className="flex justify-center items-center h-full">
+                        <h1 className="text-2xl font-bold">
+                            Nenhuma tarefa encontrada
+                        </h1>
+                    </div>
+                ) : (
+                    tasks.map((task) => (
+                        <TaskCard
+                            key={task.title}
+                            title={task.title}
+                            description={task.description}
+                            createdAt={task.createdAt}
+                            dueDate={task.dueDate}
+                            stake={task.stake}
+                        />
+                    ))
+                )}
+            </div>
         </div>
-    </div>
-);
+    )
 }
